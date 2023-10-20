bc4fd0a99feb8b6d69cacc93a1a3301f343ab600
Shawn McAdam <57196702+shawn-mcadam@users.noreply.github.com>
2023-10-20 03:22:40 +0000

    finished making A1

--- Assignments/A1/Readme.md
+++ Assignments/A1/Readme.md
@@ -1,4 +1,5 @@
 # Math 460/872 Assignment 1
+This entire assignment is contained within `act.lean`.
 
 ### Purpose
 The purpose of this assignment is to
@@ -7,13 +8,22 @@ The purpose of this assignment is to
 - Define some structures
 
 ### Part 1
-The files MoP-1 and MoP-2 contain worked examples and exercises from _Mechanics of Proof_, an online book by H.R. MacBeth which you can access here: [Mechanics of Proof](https://hrmacbeth.github.io/math2001/index.html). Some examples are incomplete because they use the `sorry` tactic. You will supply the proofs of the following 6 examples:
+The first three questions in this assignment are exercises from _Mechanics of Proof_, an online book by H.R. MacBeth which you can access here: [Mechanics of Proof](https://hrmacbeth.github.io/math2001/index.html). Supply the proofs of the three examples at the beginning of `act.lean`.
 
 ### Part 2
-To start leanifying Seven Sketches in Compositionality: An Invitation to Applied Category Theory (ACT), you will filling in the blanks left in `act.lean`. Currently, this file is a skeleton of ACT chapter 1.
+To start leanifying Seven Sketches in Compositionality: An Invitation to Applied Category Theory (ACT), you will filling in the blanks left in `act.lean`. Currently, this file is a skeleton of ACT chapter 1. Find any place with a `TODO` or `sorry` and fill in the details.
 You must
 1. Set up a namespace
-2. Define a partially ordered set
-3. Define a monotone function
-4. Prove the identity is monotone
+2. Show the DiscretePreorder is a preorder
+3. Define the Prop `monotone` which takes a function as an input
+4. Prove the identity over a preorder is monotone
 5. Prove the composition of two monotone functions is monotone
+
+### Recommended tactics
+Students are allowed to solve these problems by any means; however, this is a list of very useful tactics for solving these problems:
+- intro
+- rw \[hypothesis\]
+- calc
+- apply
+- apply?
+Make liberal use of `apply?`
--- Assignments/A1/act.lean
+++ Assignments/A1/act.lean
@@ -1,1 +1,112 @@
+import GlimpseOfLean.Library.Basic
 
+
+namespace Compute
+
+/- MoP 1.3.11 question 1. You will likely use `rw` and `ring` -/
+example {x y : ℝ} (h1 : x = 3) (h2 : y = 4 * x - 3) :
+  y = 9 := by {
+  sorry
+}
+
+/- MoP 1.3.11 question 3. You will likely use `apply?`, `rw`, and `rfl`.
+   Any time you are in doubt, use `apply?` -/
+example {x y : ℤ} (h1 : x - 3 * y = 5) (h2 : y = 3) :
+  x = 14 := by {
+  sorry
+}
+
+
+/- 1.4.11 question 6. Make liberal use of apply? for this question.
+You will also need the fact that (a-b)^2 ≥ 0 -/
+example (a b : ℝ)
+    : a ^ 2 + b ^ 2 ≥ 2 * a * b := by {
+  sorry
+}
+
+end Compute
+
+
+/- TODO define namespace ACT here so our definitions of Preorder and PartialOrder don't conflict with Mathlib's!! -/
+
+
+/- A preorder is a reflexive, transitive relation `≤` with `a < b` defined in the obvious way.
+   Note that this is _not_ a partial order because it is not necessarily symmetric.
+   Everything in a preorder need not be comparable either, because we don't have a property saying ∀ x y, 𝑥 ≤ 𝑦 ∨ 𝑦 ≤ 𝑥 -/
+class Preorder (α : Type) extends LE α, LT α where
+  le_refl : ∀ a : α, a ≤ a
+  le_trans : ∀ a b c : α, a ≤ b → b ≤ c → a ≤ c
+  lt := fun a b => a ≤ b ∧ ¬b ≤ a
+  lt_iff_le_not_le : ∀ a b : α, a < b ↔ a ≤ b ∧ ¬b ≤ a := by intros; rfl
+
+/- Preorders need not be symmetric, but ACT defines notation for when a≤b and b≤a b/c this situation is still special -/
+notation a "≅" b => a ≤ b ∧ b ≤ a
+
+/- A partial order is a reflexive, transitive, antisymmetric relation `≤`. -/
+class PartialOrder (α : Type) extends Preorder α where
+  le_antisymm : ∀ a b : α, a ≤ b → b ≤ a → a = b
+
+section Ch1
+
+/- A set P is a discrete preorder if ∀ a,b ∈ P, a ≤ b ↔ a = b
+ - A discrete preorder is a preorder: -/
+def DiscretePreorder (α : Type) : Preorder α where
+  le := fun a b => a = b
+  le_refl := by sorry
+  le_trans := by {
+    sorry
+  }
+
+/- A set P is a codiscrete preorder if ∀ a,b ∈ A, a ≤ b
+ - A codiscrete preorder is a preorder: -/
+def CodiscretePreorder (α : Type) : Preorder α where
+  le := fun a b => true
+  le_refl := by intro a; rfl
+  le_trans := by intro a b c hab hbc; rfl
+
+/- Lean 4's Bool is a Preorder (and it comes packaged with a definition for ≤ already) -/
+instance : Preorder (Bool) where
+  le_refl := by intro a; rfl
+  le_trans := by intro a b c hab hbc; apply ge_trans hbc hab
+
+
+/- Until the end of this section, A,B,C are sets whose elements might be comparable with ≤ and < -/
+variable [Preorder X]
+variable {A B C : Set X}
+
+/- TODO Fill out the definition of a monotone function -/
+def monotone (f : A → B) := ∀ x, x
+
+/- Prove the identity function is monotone -/
+example (I : A → A) (hI : ∀ x, I x = x) : monotone I := by {
+  sorry
+}
+
+/- Prove that a function composition of two monotone functions is a monotone function -/
+theorem monotone_composition {g : A → B} {f : B → C} (hf : monotone f) (hg : monotone g) :
+  monotone (f ∘ g) := by {
+  sorry
+}
+
+/- We will continue to build up to meets, joins, and Galois connections -/
+def lowerBounds (s : Set X) : Set X := { x | ∀ a, a ∈ s → x ≤ a }
+def upperBounds (s : Set X) : Set X := { x | ∀ a, a ∈ s → a ≤ x }
+
+-- a meet is a greatest lower bound is an infimum
+def isMeet (s : Set X) (x₀ : X) := ∀ x, x ∈ lowerBounds s ↔ x ≤ x₀
+
+-- a join is a least upper bound is a supremum
+def isJoin (s : Set X) (x₀ : X) := ∀ x, x ∈ upperBounds s ↔ x₀ ≤ x
+
+/- Any two meets are isomorphic -/
+example {x y : X} (h1 : isMeet A x) (h2 : isMeet A y) : x ≅ y := by {
+  constructor
+  · apply (h2 x).1
+    apply (h1 x).2
+    apply Preorder.le_refl
+  · apply (h1 y).1
+    apply (h2 y).2
+    apply Preorder.le_refl
+}
+
+end Ch1
